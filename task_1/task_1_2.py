c73d05020d8df350490c934ce850bb1e6610cbe6
Shorockhov-A <andr2409@bk.ru>
2022-04-08 15:29:44 +0300

    added task_1_1_thread module

--- task_1/task_1_2.py
+++ task_1/task_1_2.py
@@ -2,7 +2,8 @@
 2. Написать функцию host_range_ping() для перебора ip-адресов из заданного диапазона. Меняться должен только последний
 октет каждого адреса. По результатам проверки должно выводиться соответствующее сообщение.
 """
-from task_1_1 import check_ip_address, host_ping
+# from task_1_1 import check_ip_address, host_ping
+from task_1_1_thread import check_ip_address, host_ping
 
 
 def host_range_ping(get_list=False):
