43d299903080606537c93b351e07a740f473001a
Shorockhov-A <andr2409@bk.ru>
2022-04-27 12:35:48 +0300

    edited launcher_ubuntu module

--- task_7/launcher_ubuntu.py
+++ task_7/launcher_ubuntu.py
@@ -16,27 +16,29 @@ def get_subprocess(file_with_args):
     return subprocess.Popen(args, preexec_fn=os.setpgrp)
 
 
-process = []
-while True:
-    TEXT_FOR_INPUT = "Выберите действие: q - выход, s - запустить сервер, k - запустить клиенты, x - закрыть все окна: "
-    action = input(TEXT_FOR_INPUT)
+if __name__ == '__main__':
+    process = []
+    while True:
+        TEXT_FOR_INPUT = "Выберите действие: q - выход, s - запустить сервер, " \
+                         "k - запустить клиенты, x - закрыть все окна: "
+        action = input(TEXT_FOR_INPUT)
 
-    if action == "q":
-        break
+        if action == "q":
+            break
 
-    elif action == "s":
-        process.append(get_subprocess("server.py"))
+        elif action == "s":
+            process.append(get_subprocess("server.py"))
 
-    elif action == 'k':
-        print('Убедитесь, что на сервере зарегистрировано необходимо количество клиентов с паролем 123456.')
-        print('Первый запуск может быть достаточно долгим из-за генерации ключей!')
-        clients_count = int(
-            input('Введите количество тестовых клиентов для запуска: '))
-        # Запускаем клиентов:
-        for i in range(clients_count):
-            process.append(get_subprocess(f"client.py -n test{i + 1} -p 123456"))
+        elif action == 'k':
+            print('Убедитесь, что на сервере зарегистрировано необходимо количество клиентов с паролем 123456.')
+            print('Первый запуск может быть достаточно долгим из-за генерации ключей!')
+            clients_count = int(
+                input('Введите количество тестовых клиентов для запуска: '))
+            # Запускаем клиентов:
+            for i in range(clients_count):
+                process.append(get_subprocess(f"client.py -n test{i + 1} -p 123456"))
 
-    elif action == "x":
-        while process:
-            victim = process.pop()
-            os.killpg(victim.pid, signal.SIGINT)
+        elif action == "x":
+            while process:
+                victim = process.pop()
+                os.killpg(victim.pid, signal.SIGINT)
