f59f748b7945503e4b1f44e0c7b0da62c135a5b5
Shorockhov-A <andr2409@bk.ru>
2022-04-27 12:31:05 +0300

    added project files

---
+++ task_7/common/errors.py
@@ -0,0 +1,10 @@
+"""Ошибки"""
+
+
+class ServerError(Exception):
+    """Исключение - ошибка сервера."""
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return self.text
